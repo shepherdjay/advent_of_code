eb978b238b2d300df19d5b28cfdc859f79f4a678
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-06 10:43:23 -0500

    day 6 part 2 solved
    It's the same problem just with a longer queue length

--- 2022/advent_06.py
+++ 2022/advent_06.py
@@ -1,20 +1,20 @@
 from collections import deque
 
 
-def find_marker(datastream: str) -> int:
+def find_marker(datastream: str, marker_distinction: int = 4) -> int:
     """
     >>> find_marker('bvwbjplbgvbhsrlpgdmjqwftvncz')
     5
     >>> find_marker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg')
     10
     """
-    queue = deque(maxlen=4)
+    queue = deque(maxlen=marker_distinction)
 
     marker = 0
     for char in datastream:
         marker += 1
         queue.append(char)
-        if len(set(queue)) == 4:
+        if len(set(queue)) == marker_distinction:
             break
 
     return marker
@@ -24,4 +24,5 @@ if __name__ == '__main__':
     with open('advent_06_input.txt', 'r') as infile:
         datastream = infile.read().strip()
 
-    print(find_marker(datastream))
+    print(find_marker(datastream, marker_distinction=4))
+    print(find_marker(datastream, marker_distinction=14))
