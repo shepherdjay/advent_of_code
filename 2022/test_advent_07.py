e43cb86ec11898bfbf817827a10de53c847b3aab
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-07 12:30:20 -0500

    WIP: day 7 - possibly use xmltodict

--- 2022/test_advent_07.py
+++ 2022/test_advent_07.py
@@ -1,6 +1,17 @@
 from advent_07 import find_all_dir, construct_tree
+from xml.etree import ElementTree as ET
 
-TEST_INPUT = """$ cd /
+
+SIMPLE = [
+    "$ cd /",
+    "$ ls",
+    "dir a",
+    "dir b",
+    "$ cd a",
+    "$ ls",
+    "20 c.txt"]
+
+EXAMPLE_INPUT = """$ cd /
 $ ls
 dir a
 14848514 b.txt
@@ -23,7 +34,6 @@ $ ls
 8033020 d.log
 5626152 d.ext
 7214296 k"""
-
 EXAMPLE_TREE = {
     'a': {
         'e': {
@@ -45,22 +55,15 @@ EXAMPLE_TREE = {
 
 
 def test_find_all_dir():
-    example_input = TEST_INPUT
-    expected = {'a': 94853, 'e': 584}
+    example_input = SIMPLE
+    expected_simple = {'a': 20, 'c': 20}
 
-    assert find_all_dir(example_input, maxsize=10000) == expected
+    assert find_all_dir(example_input, maxsize=10000) == expected_simple
 
 
 def test_construct_tree():
-    simple_input = [
-        "$ cd /",
-        "$ ls",
-        "dir a",
-        "dir b",
-        "$ cd a",
-        "$ ls",
-        "20 c.txt"]
     simple_tree = {'a': {'c.txt': 20}, 'b': {}}
 
-    assert construct_tree(simple_input) == simple_tree
-    assert construct_tree(TEST_INPUT) == EXAMPLE_TREE
+    simple_result = construct_tree(SIMPLE)
+
+    assert simple_result == simple_tree
