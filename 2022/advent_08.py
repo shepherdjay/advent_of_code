0baa4729ec0a740f6dc89a7ea225e8df33823314
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-08 15:23:13 -0500

    day 8 part 2
    Had to fix neighbors so that trees appear in order with respect to the origin. ie. Trees farther away appear later in the list

--- 2022/advent_08.py
+++ 2022/advent_08.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple, Dict
 import numpy as np
+import math
 
 
 def extract_neighbors(forest_map: np.ndarray, coordinates: Tuple[int, int]) -> Dict[str, List[int]]:
@@ -15,7 +16,7 @@ def extract_neighbors(forest_map: np.ndarray, coordinates: Tuple[int, int]) -> D
         if col_index == y:
             pass
         elif col_index < y:
-            neighbors["left"].append(value)
+            neighbors["left"].insert(0, value)
         elif col_index > y:
             neighbors["right"].append(value)
 
@@ -25,7 +26,7 @@ def extract_neighbors(forest_map: np.ndarray, coordinates: Tuple[int, int]) -> D
         if row_index == x:
             pass
         elif row_index < x:
-            neighbors["up"].append(value)
+            neighbors["up"].insert(0, value)
         elif row_index > x:
             neighbors["down"].append(value)
     return neighbors
@@ -51,22 +52,48 @@ def is_visible(height: int, neighbor_map: dict):
     return False
 
 
+def scenic_score(height: int, neighbor_map: dict) -> int:
+    tree_counts = []
+    for direction, neighbors in neighbor_map.items():
+        trees_visible = 0
+        for neighbor in neighbors:
+            if neighbor < height:
+                trees_visible += 1
+            if neighbor >= height:
+                trees_visible += 1
+                break
+        tree_counts.append(trees_visible)
+
+    return math.prod(tree_counts)
+
+
 def find_visible_trees(forest_map: np.ndarray) -> List[Tuple]:
     """
-    Given a 2-dimensional array, find all 'visible' points on that array and return as a list of (x,y) Tuples
+    Given a 2-dimensional array, find all 'visible' points on that array and return as a list of (x,y) elements
     A point is visible if there are no higher values in its row and column, including no other points.
     """
     visible_int_points = []
 
     for row_index, row in enumerate(forest_map):
-        for col_index, value in enumerate(row):
+        for col_index, height in enumerate(row):
             point_coord = (row_index, col_index)
             neighbors = extract_neighbors(forest_map, point_coord)
-            if is_visible(value, neighbors):
+            if is_visible(height, neighbors):
                 visible_int_points.append(point_coord)
 
     return visible_int_points
 
+def score_every_tree(forest_map: np.ndarray) -> List[int]:
+    scores = []
+
+    for row_index, row in enumerate(forest_map):
+        for col_index, height in enumerate(row):
+            point_coord = (row_index, col_index)
+            neighbors = extract_neighbors(forest_map, point_coord)
+
+            scores.append(scenic_score(height, neighbors))
+
+    return scores
 
 if __name__ == '__main__':
     with open('day_08_input.txt', 'r') as elf_file:
@@ -79,3 +106,5 @@ if __name__ == '__main__':
 
     visible_trees = find_visible_trees(forest_map=forest_map)
     print(len(visible_trees))
+
+    print(max(score_every_tree(forest_map=forest_map)))
