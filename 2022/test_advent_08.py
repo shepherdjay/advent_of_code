93b13fafca3377eefc76a93269b30d14d1bbc4cb
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-08 15:23:13 -0500

    day 8 part 2
    Had to fix neighbors so that trees appear in order with respect to the origin. ie. Trees farther away appear later in the list

--- 2022/test_advent_08.py
+++ 2022/test_advent_08.py
@@ -1,6 +1,6 @@
 import pytest
 
-from advent_08 import extract_neighbors, is_visible, find_visible_trees
+from advent_08 import extract_neighbors, is_visible, find_visible_trees, scenic_score, score_every_tree
 
 EXAMPLE_FOREST_MAP = [[3, 0, 3, 7, 3],
                       [2, 5, 5, 1, 2],
@@ -10,9 +10,9 @@ EXAMPLE_FOREST_MAP = [[3, 0, 3, 7, 3],
 
 
 def test_extract_neighbors():
-    expected = {'left': [3], 'right': [3, 7, 3], 'up': [], 'down': [5, 5, 3, 5]}
+    expected = {'left': [5, 6], 'right': [3, 2], 'up': [5, 3], 'down': [5, 3]}
 
-    assert extract_neighbors(EXAMPLE_FOREST_MAP, (0, 1)) == expected
+    assert extract_neighbors(EXAMPLE_FOREST_MAP, (2, 2)) == expected
 
 
 @pytest.mark.parametrize('value,neighbors,expected_result', [
@@ -25,3 +25,12 @@ def test_is_visible(value, neighbors, expected_result):
 
 def test_find_visible_trees():
     assert len(find_visible_trees(EXAMPLE_FOREST_MAP)) == 21
+
+@pytest.mark.parametrize('height,neighbors,expected_result', [
+    (5, {"up": [3], "down": [3, 5, 3], "left": [5, 2], "right": [1, 2]}, 4)
+])
+def test_scenic_score(height, neighbors, expected_result):
+    assert scenic_score(height, neighbors) == expected_result
+
+def test_score_every_tree():
+    assert max(score_every_tree(EXAMPLE_FOREST_MAP)) == 8
