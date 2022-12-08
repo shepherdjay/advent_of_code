cf179acc8eab5e35e1f705d0aa56ad8b3ce813c3
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-08 13:58:30 -0500

    day 8 part 1 solved

--- 2022/advent_08.py
+++ 2022/advent_08.py
@@ -1,38 +1,53 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
-def extract_neighbors(forest_map: List[List[int]], coordinates: Tuple[int, int]) -> List[int]:
-    """
-    >>> forest_map = [
-    ...    [3, 0, 3, 7, 3],
-    ...    [2, 5, 5, 1, 2],
-    ...    [6, 5, 3, 3, 2],
-    ...    [3, 3, 5, 4, 9],
-    ...    [3, 5, 3, 9, 0]]
-    >>> extract_neighbors(forest_map, (0, 1))
-    [3, 0, 3, 7, 3, 5, 5, 3, 5]
-    """
-    neighbors = []
+
+def extract_neighbors(forest_map: List[List[int]], coordinates: Tuple[int, int]) -> Dict[str, List[int]]:
+    neighbors = {
+        "left": [],
+        "right": [],
+        "up": [],
+        "down": []
+    }
     x, y = coordinates
     for row_index, row in enumerate(forest_map):
         for col_index, value in enumerate(row):
-            if row_index == x or col_index == y:
-                neighbors.append(value)
-
+            if row_index == x and col_index == y:
+                pass
+            elif row_index == x and col_index < y:
+                neighbors["left"].append(value)
+            elif row_index == x and col_index > y:
+                neighbors["right"].append(value)
+            elif row_index < x and col_index == y:
+                neighbors["up"].append(value)
+            elif row_index > x and col_index == y:
+                neighbors["down"].append(value)
     return neighbors
+
+
+def is_visible(height, neighbor_map):
+    """
+    Given a height x, and a neighbor map "up, "down, "left", "right" return True if all values of a key are < x
+    An empty list is considered 0
+    """
+    for direction, neighbors in neighbor_map.items():
+        if len(neighbors) == 0:
+            neighbor_map[direction] = [float('-inf')]
+
+    visible_results = [
+        all(x < height for x in neighbor_map["left"]),
+        all(x < height for x in neighbor_map["right"]),
+        all(x < height for x in neighbor_map["up"]),
+        all(x < height for x in neighbor_map["down"]),
+    ]
+    if any(visible_results):
+        return True
+    return False
 
 
 def find_visible_trees(forest_map: List[List[int]]) -> List[Tuple]:
     """
     Given a 2-dimensional array, find all 'visible' points on that array and return as a list of (x,y) Tuples
-    A point is visible if there are no higher values in its row or column, including no other points.
-    >>> forest_map = [
-    ...    [3, 0, 3, 7, 3],
-    ...    [2, 5, 5, 1, 2],
-    ...    [6, 5, 3, 3, 2],
-    ...    [3, 3, 5, 4, 9],
-    ...    [3, 5, 3, 9, 0]]
-    >>> len(find_visible_trees(forest_map))
-    21
+    A point is visible if there are no higher values in its row and column, including no other points.
     """
     visible_int_points = []
 
@@ -40,14 +55,18 @@ def find_visible_trees(forest_map: List[List[int]]) -> List[Tuple]:
         for col_index, value in enumerate(row):
             point_coord = (row_index, col_index)
             neighbors = extract_neighbors(forest_map, point_coord)
-            for neighbor in neighbors:
-                if neighbor > value:
-                    break
-            else:
+            if is_visible(value, neighbors):
                 visible_int_points.append(point_coord)
 
-    exterior_len = len(forest_map) * len(forest_map[0])
+    print(visible_int_points)
+    return visible_int_points
 
-    return visible_int_points + exterior_len
+if __name__ == '__main__':
+    with open('day_08_input.txt', 'r') as elf_file:
+        array = []
+        for line in elf_file:
+            new_row = [int(char) for char in line.strip()]
+            array.append(new_row)
 
-
+    visible_trees = find_visible_trees(forest_map=array)
+    print(len(visible_trees))
