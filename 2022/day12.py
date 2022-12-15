1f01b216fae4dc94750f0bf509d1683cc54905d4
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-15 17:15:52 -0500

    day12 - works on a small array
    paths come back super nested on larger arrays though

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,56 +1,74 @@
 from typing import List, Tuple
 from string import ascii_letters
 from copy import deepcopy
+import itertools
 
 Elev = List[List[str]]
 
+
 def find_index(elev_map: Elev, search: str) -> Tuple[int, int]:
-    for row_idx, row  in enumerate(elev_map):
+    for row_idx, row in enumerate(elev_map):
         for col_idx, char in enumerate(row):
             if char == search:
                 return row_idx, col_idx
 
+
 def get_candidate_neighbors(elev_map: Elev, current_idx):
     row_idx, col_idx = current_idx
-    
+
     up_idx = row_idx - 1, col_idx
     down_idx = row_idx + 1, col_idx
     left_idx = row_idx, col_idx - 1
     right_idx = row_idx, col_idx + 1
 
-    neighbor_indexes = [(a, b) for (a, b) in [up_idx, down_idx, left_idx, right_idx] if a >= 0 and a < len(elev_map[0]) and b >= 0 and b < len(elev_map[0])]
+    neighbor_indexes = []
+
+    for a, b in [up_idx, down_idx, left_idx, right_idx]:
+        if 0 <= a < len(elev_map) and 0 <= b < len(elev_map[0]):
+            neighbor_indexes.append((a,b))
 
     cur_char_value = ascii_letters.find(elev_map[row_idx][col_idx])
     candidates = []
     for neighbor in neighbor_indexes:
         neigh_row_idx, neigh_col_idx = neighbor
-        if elev_map[neigh_row_idx][neigh_col_idx].islower():
+        if elev_map[neigh_row_idx][neigh_col_idx] == 'S' or elev_map[neigh_row_idx][neigh_col_idx] == 'E': # we need to make sure to always include these neighbors
+            candidates.append(neighbor)
+        else:
             neigh_char_value = ascii_letters.find(elev_map[neigh_row_idx][neigh_col_idx])
             if neigh_char_value <= cur_char_value + 1:
                 candidates.append(neighbor)
 
     return candidates
 
+def flatten_to_lists(super_nested):
+    pass
 
-def traverse_path(elev_map, starting_index, goal_index, visited=None) -> List[Tuple[int, int]]:
+
+
+def traverse_path(elev_map, starting_index, goal_index, visited=None) -> List[List[Tuple[int, int]]]:
+    paths = []
     if visited is None:
         visited = [starting_index]
     else:
         visited = deepcopy(visited)
+        visited.append(starting_index)
 
     neighbors = [x for x in get_candidate_neighbors(elev_map, starting_index) if x not in visited]
 
     for neighbor in neighbors:
-        neigh_row_idx, neigh_col_idx = neighbor
-        if elev_map[neigh_row_idx][neigh_col_idx] == goal_index:
+        if neighbor == goal_index:
             visited.append(neighbor)
             return visited
         else:
-            return traverse_path(elev_map, neighbor, goal_index, visited=visited)
-
-    
+            paths.append(traverse_path(elev_map, neighbor, goal_index, visited=visited))
+    return paths
 
 
 def process_elev_map(elev_map: Elev):
     starting_index = find_index(elev_map, 'S')
     ending_index = find_index(elev_map, 'E')
+
+    paths = traverse_path(elev_map, starting_index=starting_index, goal_index=ending_index)
+    by_length = sorted(paths, key=len)
+
+    return len(by_length[0]) - 1
--- 2022/test_day12.py
+++ 2022/test_day12.py
@@ -1,5 +1,10 @@
-from day12 import process_elev_map, find_index, get_candidate_neighbors, traverse_path
+from day12 import process_elev_map, find_index, get_candidate_neighbors, traverse_path, flatten_to_lists
 
+SUPER_SIMPLE_MAP = [
+    ['S', 'a'],
+    ['a', 'E'],
+    ['b', 'c']
+]
 
 EXAMPLE_ELEV_MAP = []
 with open('test_day12_input.txt', 'r') as infile:
@@ -9,29 +14,45 @@ with open('test_day12_input.txt', 'r') as infile:
 
 
 def test_find_index():
-    assert find_index(EXAMPLE_ELEV_MAP, 'S') == (0,0)
+    assert find_index(EXAMPLE_ELEV_MAP, 'S') == (0, 0)
 
-def test_process_file():
-    assert process_elev_map(EXAMPLE_ELEV_MAP)
 
 def test_get_candidate_neighbors():
     elev_map = EXAMPLE_ELEV_MAP
     current_idx = (2, 1)
-    expected_neighbors = [(2, 0), (1, 1 ), (3,1), (2,2)]
+    expected_neighbors = [(2, 0), (1, 1), (3, 1), (2, 2)]
 
     actual = get_candidate_neighbors(elev_map=elev_map, current_idx=current_idx)
 
     assert set(actual) == set(expected_neighbors)
 
+
 def test_traverse_paths():
-    super_simple = [
-        ['S', 'a'],
-        ['a', 'E']
-    ]
+    super_simple = SUPER_SIMPLE_MAP
 
     expected_paths = [
-        [(0,0), (0, 1), (1, 1)],
-        [(0,0), (1, 0), (1, 1)]
+        [(0, 0), (0, 1), (1, 1)],
+        [(0, 0), (1, 0), (1, 1)],
     ]
 
-    assert traverse_path(elev_map=super_simple, starting_index=(0,0), goal_index=(1,1)) == expected_paths
+    actual_paths = traverse_path(elev_map=super_simple, starting_index=(0, 0), goal_index=(1, 1))
+
+    for path in expected_paths:
+        assert path in actual_paths
+
+def test_flatten_to_lists():
+    super_nested = [
+        [[[[1,2,3,4]]]],
+        [1,2,3,4,[1,2,3,4]],
+    ]
+    expected = [
+        [1,2,3,4], [1,2,3,4], [1,2,3,4]
+    ]
+
+    assert flatten_to_lists(super_nested) == expected
+
+def test_process_simple_map():
+    assert process_elev_map(SUPER_SIMPLE_MAP) == 2
+
+def test_process_example_map():
+    assert process_elev_map(EXAMPLE_ELEV_MAP) == 31
