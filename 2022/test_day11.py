d8035ed6a1671a24305c55c1d046bc2398679964
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-14 21:57:13 -0500

    day 11 part 2 solved... modular arithmetic... yuck

--- 2022/test_day11.py
+++ 2022/test_day11.py
@@ -12,20 +12,20 @@ def test_process_a_monkey():
     with open('day11_single_monkey.txt', 'r') as infile:
         single_monkey = infile.read()
 
-    expected = Monkey.from_monkey_block(single_monkey)
+    actual = Monkey.from_monkey_block(single_monkey)
 
-    assert expected.items == [79, 98]
-    assert expected.modify_worry(current_worry=10) == 190
-    assert expected.test.divisor == 23
-    assert expected.test.true == 2
-    assert expected.test.false == 3
+    assert actual.items == [79, 98]
+    assert actual.modify_worry(current_worry=10) == 190 % 23
+    assert actual.test.divisor == 23
+    assert actual.test.true == 2
+    assert actual.test.false == 3
 
 
 def test_monkey_takes_turn():
     throwing_monkey = Monkey(items=[79, 98], operator=('*', 19), test=MonkeyTest(23, 1, 2))
 
-    buffer_monkey = Monkey(items=[], operator=None, test=None)
-    receiving_monkey = Monkey(items=[], operator=None, test=None)
+    buffer_monkey = Monkey(items=[], operator=None, test=MonkeyTest(23, 1, 2))
+    receiving_monkey = Monkey(items=[], operator=None, test=MonkeyTest(23, 1, 2))
 
     throwing_monkey.take_turn()
 
@@ -41,7 +41,7 @@ def test_process_example_monkey_file():
 def test_process_example_monkey_file_lots_of_rounds():
     process_monkey_file('test_day11_input.txt', num_rounds=5_000, worry_divisor=1)
 
-    assert Monkey.monkeys[0].inspected_items == 26075
-    assert Monkey.monkeys[1].inspected_items == 23921
-    assert Monkey.monkeys[2].inspected_items == 974
-    assert Monkey.monkeys[3].inspected_items == 26000
+    assert Monkey.monkeys[0].items_inspected == 26075
+    assert Monkey.monkeys[1].items_inspected == 23921
+    assert Monkey.monkeys[2].items_inspected == 974
+    assert Monkey.monkeys[3].items_inspected == 26000
