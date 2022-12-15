4e68d427cafd828f09836c6396b62b4ddfaac67a
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-14 21:18:43 -0500

    day 11 part 1 solved

--- 2022/test_day11.py
+++ 2022/test_day11.py
@@ -1,6 +1,10 @@
 import pytest
+from day11 import Monkey, MonkeyTest, process_monkey_file
 
-from day11 import Monkey
+@pytest.fixture(autouse=True, scope='function')
+def cleanup_monkey():
+    yield
+    Monkey.monkeys = []
 
 def test_process_a_monkey():
     with open('day11_single_monkey.txt', 'r') as infile:
@@ -8,8 +12,25 @@ def test_process_a_monkey():
 
     expected = Monkey.from_monkey_block(single_monkey)
 
-    assert expected.starting_items == [79, 98]
+    assert expected.items == [79, 98]
     assert expected.modify_worry(current_worry=10) == 190
-    assert expected.test(current_worry=23) == (True, 2)
-    assert expected.test(current_worry=24) == (False, 3)
+    assert expected.test.divisor == 23
+    assert expected.test.true == 2
+    assert expected.test.false == 3
 
+def test_monkey_takes_turn():
+    throwing_monkey = Monkey(items=[79, 98], operator=('*', 19), test=MonkeyTest(23, 1, 2))
+
+    buffer_monkey = Monkey(items=[], operator=None, test=None)
+    receiving_monkey = Monkey(items=[], operator=None, test=None)
+
+    throwing_monkey.take_turn()
+
+    assert throwing_monkey.items == []
+    assert buffer_monkey.items == []
+    assert receiving_monkey.items == [500, 620]
+
+
+def test_process_example_monkey_file():
+
+    assert process_monkey_file('test_day11_input.txt') == 10605
