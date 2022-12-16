dde307f41528b71843ff051b8f3c650fce343e09
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-16 17:19:58 -0500

    day13 part 1 ez

---
+++ 2022/test_day13.py
@@ -0,0 +1,19 @@
+import pytest
+from day13 import comparator, process_file
+
+
+@pytest.mark.parametrize('a,b,expected', [
+    ([1,1,3,1,1], [1,1,5,1,1],  True),
+    ([[1],[2,3,4]], [[1],4], True),
+    ([9], [[8,7,6]], False),
+    ([[4,4],4,4], [[4,4],4,4,4], True),
+    ([7,7,7,7],[7,7,7], False),
+    ([], [3], True),
+    ([[[]]],[[]], False),
+    ([1,[2,[3,[4,[5,6,7]]]],8,9],[1,[2,[3,[4,[5,6,0]]]],8,9], False)
+])
+def test_comparator(a,b,expected):
+    assert comparator(a,b) == expected
+
+def test_process_file():
+    assert process_file('test_day13_input.txt') == 13
