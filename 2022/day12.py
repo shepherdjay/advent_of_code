2d264e2a41df7191a4ef48fb8a50b5a633871250
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-15 20:29:27 -0500

    working for sample but not completing for part 1

--- 2022/day12.py
+++ 2022/day12.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 import itertools
 
 Elev = List[List[str]]
+HeightMap = List[List[int]]
 
 
 def find_index(elev_map: Elev, search: str) -> Tuple[int, int]:
@@ -13,7 +14,35 @@ def find_index(elev_map: Elev, search: str) -> Tuple[int, int]:
                 return row_idx, col_idx
 
 
-def get_candidate_neighbors(elev_map: Elev, current_idx):
+def transform_elev_map(elev_map: Elev):
+    """
+    Returns an elevation map with the letters replaced by integer values.
+    'S' is set to 0
+    'E' is set to the maximum integer of all other characters
+    """
+
+    flatten_list = list(itertools.chain.from_iterable(elev_map))
+    largest_lower_case_letter = sorted(list(set(flatten_list)), reverse=True)[0]
+    e_value = ascii_letters.find(largest_lower_case_letter)
+
+    new_elev_map = []
+    for row in elev_map:
+        new_row = []
+        for char in row:
+            match char:
+                case 'S':
+                    char_value = 0
+                case 'E':
+                    char_value = e_value
+                case other:
+                    char_value = ascii_letters.find(char)
+            new_row.append(char_value)
+        new_elev_map.append(new_row)
+
+    return new_elev_map
+
+
+def get_candidate_neighbors(elev_map: HeightMap, current_idx):
     row_idx, col_idx = current_idx
 
     up_idx = row_idx - 1, col_idx
@@ -25,41 +54,33 @@ def get_candidate_neighbors(elev_map: Elev, current_idx):
 
     for a, b in [up_idx, down_idx, left_idx, right_idx]:
         if 0 <= a < len(elev_map) and 0 <= b < len(elev_map[0]):
-            neighbor_indexes.append((a,b))
+            neighbor_indexes.append((a, b))
 
-    cur_char = elev_map[row_idx][col_idx]
-    if cur_char == 'S':
-        cur_char_value = 0
-    else:
-        cur_char_value = ascii_letters.find(cur_char)
     candidates = []
+    cur_value = elev_map[row_idx][col_idx]
     for neighbor in neighbor_indexes:
         neigh_row_idx, neigh_col_idx = neighbor
-        neigh_char = elev_map[neigh_row_idx][neigh_col_idx]
-        if neigh_char == 'S':
-            neigh_char_value = 0
-        elif neigh_char == 'E':
-            neigh_char_value = 25
-        else:
-            neigh_char_value = ascii_letters.find(neigh_char)
-        if neigh_char_value in [cur_char_value, cur_char_value - 1, cur_char_value + 1]:
+        neigh_value = elev_map[neigh_row_idx][neigh_col_idx]
+
+        if neigh_value <= cur_value + 1:
             candidates.append(neighbor)
 
     return candidates
 
+
 def flatten_paths(super_nests):
     items = []
-    
+
     for x in super_nests:
         if isinstance(x, list):
             items.extend(flatten_paths(x))
         else:
             items.append(x)
-    
+
     return items
 
 
-def traverse_path(elev_map, starting_index, goal_index, visited=None) -> List[List[Tuple[int, int]]]:
+def traverse_path(elev_map, starting_index, goal_index, visited=None) -> List:
     paths = []
     if visited is None:
         visited = [starting_index]
@@ -82,7 +103,9 @@ def process_elev_map(elev_map: Elev):
     starting_index = find_index(elev_map, 'S')
     ending_index = find_index(elev_map, 'E')
 
-    paths = traverse_path(elev_map, starting_index=starting_index, goal_index=ending_index)
+    height_map = transform_elev_map(elev_map)
+
+    paths = traverse_path(height_map, starting_index=starting_index, goal_index=ending_index)
     paths = flatten_paths(paths)
 
     unflatten = []
@@ -95,3 +118,15 @@ def process_elev_map(elev_map: Elev):
     by_length = sorted(unflatten, key=len)
     print(by_length[0])
     return len(by_length[0]) - 1
+
+
+if __name__ == '__main__':
+    problem_input = []
+    with open('day12_input.txt', 'r') as infile:
+        rows = [line.strip() for line in infile]
+        for row in rows:
+            problem_input.append([char for char in row])
+
+    # part1
+    num_of_steps = process_elev_map(problem_input)
+    print(num_of_steps)
