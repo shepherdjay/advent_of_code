09eebc7fa7dc2a7b1ea38023c449bc6fe79d52c2
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-19 17:17:44 -0500

    I went down a dark and lonely road

--- 2022/16/day16.py
+++ 2022/16/day16.py
@@ -1,12 +1,49 @@
 from dataclasses import dataclass, field
+import heapq
+import itertools
+import re
 
 
 class ValveTree(list):
     """ This exists to support easy index reference by string """
-
     def __init__(self, valves: list):
         self.valves = valves
+        self._spt = None
         super().__init__(valves)
+
+    @property
+    def spt(self):
+        if self._spt is None:
+            self.generate_spt()
+        return self._spt
+
+    def generate_spt(self):
+        shortest_tree = {}
+        for valve in self.valves:
+            starting = valve
+            node_map = {node: (float('inf'), []) for node in self.valves}
+            node_map[starting] = (0, [starting])
+            min_dist = [(0, starting)]
+            visited = set()
+
+            while min_dist:
+                cur_dist, cur = heapq.heappop(min_dist)
+                if cur in visited:
+                    continue
+                visited.add(cur)
+
+                for neighbor_id in cur.neighbors:
+                    neighbor = self.__getitem__(neighbor_id)
+                    if neighbor in visited:
+                        continue
+                    this_dist = cur_dist + 1
+                    if this_dist < node_map[neighbor][0]:
+                        node_map[neighbor] = (this_dist, node_map[cur][1] + [neighbor])
+                        heapq.heappush(min_dist, (this_dist, neighbor))
+            shortest_tree[valve] = node_map
+        self._spt = shortest_tree
+        return shortest_tree
+
 
     def __getitem__(self, item: str | int):
         if isinstance(item, int):
@@ -16,24 +53,92 @@ class ValveTree(list):
                 return valve
 
 
-@dataclass
+@dataclass(order=True)
 class Valve:
-    id: str = field(compare=True, hash=True)
-    flow: int
+    id: str = field()
+    flow: int = field()
     neighbors: set[str] | None = field(default_factory=set)
 
+    def __hash__(self):
+        return hash(self.__repr__())
 
-def relieve_pressure(valve_tree, timer: int):
+class Erupt(Exception):
     pass
 
+class Volcano:
+    def __init__(self, timer):
+        self.timer = timer
+        self.relief = 0
+        self.open_valves = list()
 
-def find_common_ancester(valves: list[str], valve_tree: ValveTree):
-    pass
+    def _tick(self):
+        self.relief += sum((valve.flow for valve in self.open_valves))
+        self.timer -= 1
+        if self.timer == 0:
+            raise Erupt
+
+    def move(self, i):
+        for _ in range(i):
+            self._tick()
+
+    def open_valve(self, valve: Valve):
+        self._tick()
+        self.open_valves.append(valve)
 
 
-def path_between_two_nodes(a_node, z_node, tree: ValveTree):
-    pass
+
+def get_relief_nodes(tree):
+    return [node for node in tree if node.flow > 0]
+
+def simulate_path(path, relief_nodes, timer):
+    volcano = Volcano(timer=timer)
+    try:
+        for valve in path:
+            volcano.move()
+            if valve in relief_nodes:
+                volcano.open_valve(valve)
+        for i in range(timer - volcano.timer):
+            volcano.move()
+    except Erupt:
+        return volcano.relief, path
+
+def relieve_pressure(valve_tree, timer: int, starting_node_id: str='A'):
+    relief_nodes = get_relief_nodes(valve_tree)
+    paths = generate_possible_paths(starting_node_id=starting_node_id, valve_tree=valve_tree)
+
+    relief_paths = [simulate_path(path, relief_nodes=relief_nodes, timer=timer) for path in paths]
 
 
-def generate_possible_paths(starting_node, valve_tree: ValveTree):
-    pass
+    relief_paths.sort(reverse=True)
+    best_path = relief_paths[0]
+    return best_path[0], [valve.id for valve in best_path[1]]
+
+def cost_between_nodes(a_node_id: str, z_node_id: str, tree: ValveTree):
+    a_node = tree[a_node_id]
+    z_node = tree[z_node_id]
+    return tree.spt[a_node][z_node][0]
+
+def generate_possible_paths(starting_node_id, valve_tree: ValveTree):
+    relief_nodes = get_relief_nodes(valve_tree)
+    relief_node_ordering = map(list, itertools.permutations(relief_nodes))
+
+    paths = []
+    for path in relief_node_ordering:
+        path.insert(0, valve_tree[starting_node_id])
+        full_path = []
+        for index, valve in enumerate(path):
+            try:
+                next_valve = path[index + 1]
+                node_path = valve_tree.spt[valve][next_valve][1][1:]
+                full_path.extend(node_path)
+            except IndexError:
+                continue
+        paths.append(full_path)
+
+    return paths
+
+def parse_line(line:str):
+    valve_regex = re.compile(r'Valve (.+) has flow rate=(\d+); tunnels? leads? to valves? (.+)')
+    valve_id, flow_rate, neighbors_raw = valve_regex.search(line).groups()
+    neighbors = neighbors_raw.split(', ')
+    return Valve(id=valve_id, flow=int(flow_rate), neighbors=set(neighbors))
--- 2022/16/test_day16.py
+++ 2022/16/test_day16.py
@@ -1,5 +1,5 @@
 import pytest
-from day16 import Valve, ValveTree, relieve_pressure, generate_possible_paths, path_between_two_nodes
+from day16 import Valve, ValveTree, relieve_pressure, generate_possible_paths, cost_between_nodes, parse_line, simulate_path
 
 
 @pytest.fixture(scope='function')
@@ -13,9 +13,31 @@ def simple_valve_tree():
     ])
 
 
+@pytest.fixture(scope='function')
+def example_valve_tree():
+    with open('day16_example_input.txt', 'r') as infile:
+        valves = [parse_line(line.strip()) for line in infile]
+    return ValveTree(valves)
+
+
 def test_simple_valve_tree(simple_valve_tree):
     """ GIVEN: The simple_valve_tree and 10 Minutes """
-    assert relieve_pressure(valve_tree=simple_valve_tree, timer=10) == (144, ['A', 'C', 'A', 'B', 'D'])
+    assert relieve_pressure(valve_tree=simple_valve_tree, timer=10, starting_node_id='A') == (144, ['C', 'A', 'B', 'D'])
+
+
+def test_parse_line():
+    assert parse_line('Valve AA has flow rate=0; tunnels lead to valves DD, II, BB') \
+           == Valve(id='AA', flow=0, neighbors={'DD', 'II', 'BB'})
+
+
+def test_example_valve_tree(example_valve_tree):
+    pressure, path = relieve_pressure(valve_tree=example_valve_tree, timer=30, starting_node_id='AA')
+    assert path == ["DD", "CC", "BB", "AA",
+                    "II", "JJ", "II", "AA",
+                    "DD", "EE", "FF", "GG",
+                    "HH", "GG", "FF", "EE",
+                    "DD", "CC", ]
+    assert pressure == 1651
 
 
 def test_generate_possible_paths(simple_valve_tree):
@@ -23,19 +45,27 @@ def test_generate_possible_paths(simple_valve_tree):
     GIVEN: The simple valve_tree calculate all possible combinations of paths
     that could be taken to all nodes with positive flow
     """
-    assert generate_possible_paths(starting_node='A', valve_tree=simple_valve_tree) == [
+    possible_paths = generate_possible_paths(starting_node_id='A', valve_tree=simple_valve_tree)
+    by_id = [[valve.id for valve in path] for path in possible_paths]
+    assert by_id == [
         ['C', 'A', 'B', 'D'],
         ['B', 'D', 'B', 'A', 'C']
     ]
 
 
-def test_path_between_nodes():
-    assert path_between_two_nodes('C', 'D', simple_valve_tree) == ['A', 'B']
+def test_simulate_path(example_valve_tree):
+    example_path = ["AA", "DD", "CC", "BB", "AA",
+                    "II", "JJ", "II", "AA",
+                    "DD", "EE", "FF", "GG",
+                    "HH", "GG", "FF", "EE",
+                    "DD", "CC", ]
+
+    node_path = [example_valve_tree[node_id] for node_id in example_path]
+    relief_nodes = [example_valve_tree[node_id] for node_id in ['BB', 'CC', 'DD', 'EE', 'HH', 'JJ']]
+
+    path_cost, path = simulate_path(node_path, relief_nodes=relief_nodes, timer=30)
+    assert path_cost == 1651
 
 
-def test_find_common_ancester(simple_valve_tree):
-    """ GIVEN: The simple_valve_tree with an additional relationship between B and C """
-    simple_valve_tree['B'].neighbors.add('C')
-    simple_valve_tree['C'].neighbors.add('B')
-
-    assert find_common_ancester(['C', 'D'], valve_tree=simple_valve_tree) == 'B'
+def test_cost_between_nodes(simple_valve_tree):
+    assert cost_between_nodes('C', 'D', simple_valve_tree) == 3
