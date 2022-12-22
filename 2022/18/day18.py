cc787f1edd332c709a98dfbc3f36cdd4163b5705
Jay Shepherd <shepherdjay@users.noreply.github.com>
2022-12-22 10:16:32 -0500

    simplify function make code more readable

--- 2022/18/day18.py
+++ 2022/18/day18.py
@@ -1,12 +1,11 @@
-from collections import deque
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+from collections import deque, namedtuple
 
 EXAMPLE_CUBES = [(2, 2, 2), (1, 2, 2), (3, 2, 2), (2, 1, 2), (2, 3, 2), (2, 2, 1), (2, 2, 3), (2, 2, 4), (2, 2, 6),
                  (1, 2, 5), (3, 2, 5), (2, 1, 5), (2, 3, 5)]
 
 Coord = tuple[int, int, int]
+
+Dimensions = namedtuple('Dimensions', 'x_min,x_max,y_min,y_max,z_min,z_max')
 
 
 def surface_area(cubes):
@@ -30,84 +29,59 @@ def surface_area(cubes):
             exposed_sides[(x, y, z + 1)] -= 1
     return sum(exposed_sides.values())
 
-def visualization(box):
-    x_values = [cube[0] for cube in box]
-    y_values = [cube[1] for cube in box]
-    z_values = [cube[2] for cube in box]
+
+def get_dimensions(array):
+    x_values = [i[0] for i in array]
+    y_values = [i[1] for i in array]
+    z_values = [i[2] for i in array]
     min_x, max_x = min(x_values), max(x_values)
     min_y, max_y = min(y_values), max(y_values)
     min_z, max_z = min(z_values), max(z_values)
+    return Dimensions(min_x, max_x, min_y, max_y, min_z, max_z)
 
-    axes = [
-        max_x - min_x,
-        max_y - min_y,
-        max_z - min_z
-    ]
-
-    # Create Data
-    data = [np.ones(cube, dtype=np.bool_) for cube in cubes]
-
-    # Control Transparency
-    alpha = 0.9
-
-    # Control colour
-    colors = np.empty(axes + [4], dtype=np.float32)
-
-    colors[:] = [0, 0, 0, alpha]  # red
-
-    # Plot figure
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    # Voxels is used to customizations of the
-    # sizes, positions and colors.
-    ax.voxels(data, facecolors=colors)
-    plt.show()
 
 def explore_exterior(cubes):
     """
+    >>> explore_exterior({(1, 1, 1), (2, 1, 1), (1, 2, 1), (2, 2, 1), (1, 1, 2), (2, 1, 2), (1, 2, 2), (2, 2, 2)})
+    24
     >>> explore_exterior(EXAMPLE_CUBES)
-    1
+    58
     """
     # Find the dimensions of the object
-    x_values = [cube[0] for cube in cubes]
-    y_values = [cube[1] for cube in cubes]
-    z_values = [cube[2] for cube in cubes]
-    min_x, max_x = min(x_values), max(x_values)
-    min_y, max_y = min(y_values), max(y_values)
-    min_z, max_z = min(z_values), max(z_values)
+    cubes_dim = get_dimensions(cubes)
 
     # Create a 3D box one larger than the dimensions of the object
-    box = [(x, y, z) for z in range(min_z, max_z + 1) for y in range(min_y, max_y +1) for x in range(min_x, max_x + 1)]
-    visualization(cubes)
+    box = [(x, y, z)
+           for z in range(cubes_dim.z_min - 1, cubes_dim.z_max + 2)
+           for y in range(cubes_dim.y_min - 1, cubes_dim.y_max + 2)
+           for x in range(cubes_dim.x_min - 1, cubes_dim.x_max + 2)]
 
     # Find the bottom left corner of the 3D box
-    x_values = [cube[0] for cube in box]
-    y_values = [cube[1] for cube in box]
-    z_values = [cube[2] for cube in box]
-    min_x, max_x = min(x_values), max(x_values)
-    min_y, max_y = min(y_values), max(y_values)
-    min_z, max_z = min(z_values), max(z_values)
+    box_dim = get_dimensions(box)
 
     visited = set()
-    queue = deque([(min_x, min_y, min_z)])
+    queue = deque([(box_dim.x_min, box_dim.y_min, box_dim.z_min)])
+    object_surface_area = 0
 
     while queue:
         node = queue.popleft()
         if node in visited:
             continue
         visited.add(node)
-        if node not in cubes:
-            neighbors = calculate_neighbors(node)
-            for neighbor in neighbors:
-                if neighbor not in visited and neighbor not in cubes:
-                    queue.append(neighbor)
+        neighbors = calculate_neighbors(node)
+        for neighbor in neighbors:
+            if neighbor in cubes:
+                object_surface_area += 1
+                continue
+            if neighbor[0] < box_dim.x_min or neighbor[0] > box_dim.x_max:
+                continue
+            if neighbor[1] < box_dim.y_min or neighbor[1] > box_dim.y_max:
+                continue
+            if neighbor[2] < box_dim.z_min or neighbor[2] > box_dim.z_max:
+                continue
+            queue.append(neighbor)
+    return object_surface_area
 
-    # Calculate the surface area of the object by counting the number of exposed cells
-    exterior_area = 2 * ((max_x - min_x + 1) * (max_y - min_y + 1) + (max_x - min_x + 1) * (max_z - min_z + 1) + (
-                max_y - min_y + 1) * (max_z - min_z + 1))
-
-    return exterior_area
 
 def calculate_neighbors(cube):
     """
@@ -120,20 +94,18 @@ def calculate_neighbors(cube):
     return ((x + dx, y + dy, z + dz) for dx, dy, dz in offsets)
 
 
-with open('day18_input.txt', 'r') as cube_file:
-    puzzle = []
-    for line in cube_file:
-        clean_line = line.strip().split(',')
-        x, y, z = clean_line
-        puzzle.append(
-            (int(x), int(y), int(z))
-        )
+if __name__ == '__main__':
+    with open('day18_input.txt', 'r') as cube_file:
+        puzzle = []
+        for line in cube_file:
+            clean_line = line.strip().split(',')
+            x, y, z = clean_line
+            puzzle.append(
+                (int(x), int(y), int(z))
+            )
 
-    simplest = {
-        (1, 1, 1), (2, 1, 1), (1, 2, 1), (2, 2, 1), (1, 1, 2), (2, 1, 2), (1, 2, 2), (2, 2, 2)
-    }
-
-    for cubes in [simplest]:
-        total_surface_area = surface_area(cubes)
-        exposed = explore_exterior(cubes)
-        print(f"For {len(cubes)} cubes. The total surface area is {total_surface_area}. The exposed surface area is {exposed}")
+        for cubes in [EXAMPLE_CUBES, puzzle]:
+            total_surface_area = surface_area(cubes)
+            exposed = explore_exterior(cubes)
+            print(
+                f"For {len(cubes)} cubes. The total surface area is {total_surface_area}. The exposed surface area is {exposed}")
