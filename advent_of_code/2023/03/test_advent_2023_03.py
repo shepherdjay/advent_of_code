128a6f9da512a40ddfe4d822d8a2143ae29030b0
Jay Shepherd <shepherdjay@users.noreply.github.com>
2023-12-03 01:13:54 -0500

    black formatted

--- advent_of_code/2023/03/test_advent_2023_03.py
+++ advent_of_code/2023/03/test_advent_2023_03.py
@@ -1,7 +1,7 @@
 from advent_2023_03 import process_schematic, find_neighbors, extract_numbers_start_end
 
 
-EXAMPLE_SCHEMATIC = '''467..114..
+EXAMPLE_SCHEMATIC = """467..114..
 ...*......
 ..35..633.
 ......#...
@@ -10,21 +10,27 @@ EXAMPLE_SCHEMATIC = '''467..114..
 ..592.....
 ......755.
 ...$.*....
-.664.598..'''
+.664.598.."""
+
 
 def test_process_schematic():
     assert process_schematic(EXAMPLE_SCHEMATIC.splitlines()) == 4361
 
+
 def test_find_neighbors():
     exp_neighbors = {
-        (0, 2), (0, 3), (0, 4),
-        (1, 2),         (1, 4),
-        (2, 2), (2, 3), (2, 4),
+        (0, 2),
+        (0, 3),
+        (0, 4),
+        (1, 2),
+        (1, 4),
+        (2, 2),
+        (2, 3),
+        (2, 4),
     }
     assert find_neighbors((1, 3)) == exp_neighbors
 
+
 def test_extract_numbers_start_end():
-    sample_row = ['467..114..']
-    expected = [
-        (467, 0, 2), (114, 5, 7)
-    ]
+    sample_row = ["467..114.."]
+    expected = [(467, 0, 2), (114, 5, 7)]
