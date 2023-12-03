b1c21116f375f35b66c6335538043e33635da12a
Jay Shepherd <shepherdjay@users.noreply.github.com>
2023-12-03 01:12:08 -0500

    part1 - tests pass but real answer too high?

---
+++ advent_of_code/2023/03/test_advent_2023_03.py
@@ -0,0 +1,30 @@
+from advent_2023_03 import process_schematic, find_neighbors, extract_numbers_start_end
+
+
+EXAMPLE_SCHEMATIC = '''467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..'''
+
+def test_process_schematic():
+    assert process_schematic(EXAMPLE_SCHEMATIC.splitlines()) == 4361
+
+def test_find_neighbors():
+    exp_neighbors = {
+        (0, 2), (0, 3), (0, 4),
+        (1, 2),         (1, 4),
+        (2, 2), (2, 3), (2, 4),
+    }
+    assert find_neighbors((1, 3)) == exp_neighbors
+
+def test_extract_numbers_start_end():
+    sample_row = ['467..114..']
+    expected = [
+        (467, 0, 2), (114, 5, 7)
+    ]
