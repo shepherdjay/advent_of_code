b3b58df8d90628ff5e3240849c28ec0ea76150d0
Jay Shepherd <shepherdjay@users.noreply.github.com>
2023-12-03 01:13:54 -0500

    black formatted

--- advent_of_code/2023/03/advent_2023_03.py
+++ advent_of_code/2023/03/advent_2023_03.py
@@ -1,15 +1,15 @@
 from typing import Tuple
 import re
 
+
 def extract_numbers_start_end(row_string: str) -> list[tuple[int, int, int]]:
-    number_re = re.compile('\d+')
+    number_re = re.compile("\d+")
 
     values = []
     for match in number_re.finditer(row_string):
-        values.append(
-            (int(match.group()), match.start(), match.end())
-        )
+        values.append((int(match.group()), match.start(), match.end()))
     return values
+
 
 def find_neighbors(coord: Tuple) -> set[Tuple]:
     neighbors = set()
@@ -17,19 +17,19 @@ def find_neighbors(coord: Tuple) -> set[Tuple]:
     for row in range(row_orig - 1, row_orig + 2):
         for col in range(col_orig - 1, col_orig + 2):
             if row >= 0 and col >= 0:
-                neighbors.add((row,col))
+                neighbors.add((row, col))
     neighbors.remove(coord)
     return neighbors
 
-def process_schematic(schematic: list[str]) -> int:
 
+def process_schematic(schematic: list[str]) -> int:
     part_coord_set = set()
 
     all_values = []
     for row_id, row in enumerate(schematic):
         all_values.append(extract_numbers_start_end(row))
         for col_id, char in enumerate(row):
-            if char.isdigit() or char == '.':
+            if char.isdigit() or char == ".":
                 continue
             char_neighbors = find_neighbors((row_id, col_id))
             part_coord_set.update(char_neighbors)
@@ -43,10 +43,8 @@ def process_schematic(schematic: list[str]) -> int:
                     break
     return total
 
-if __name__ == "__main__": # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
     with open("advent_2023_03_input.txt", "r") as infile:
         schematic = infile.readlines()
     print(process_schematic(schematic))
-
-
-
