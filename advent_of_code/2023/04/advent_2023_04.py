c2866cb9e1217b7dfb7d9825a51e886a49f39e45
Jay Shepherd <shepherdjay@users.noreply.github.com>
2023-12-05 08:13:43 -0500

    wip: looping forever updating in place, maybe a dict

--- advent_of_code/2023/04/advent_2023_04.py
+++ advent_of_code/2023/04/advent_2023_04.py
@@ -1,5 +1,6 @@
+from typing import Tuple
 
-def count_card(winning_numbers: set, player_numbers: set) -> int:
+def count_card(winning_numbers: set, player_numbers: set) -> Tuple[int,int]:
     """
     Returns winnings, first match is worth 1 point for each subsequent its 2x
     >>> count_card({41, 48, 83, 86, 17}, {83, 86, 6, 31, 17, 9, 48, 53})
@@ -7,27 +8,45 @@ def count_card(winning_numbers: set, player_numbers: set) -> int:
     >>> count_card({41, 92, 73, 84, 69}, {59, 84, 76, 51, 58,  5, 54, 83})
     1
     """
-    return 2**len(winning_numbers.intersection(player_numbers)) // 2
+    matches = len(winning_numbers.intersection(player_numbers))
+    return 2**matches // 2, matches
 
-
-def process_cards(card_list: list[str]) -> int:
+def update_for_matches(idx, matches, my_list):
     """
-    >>> process_cards([ \
+    updates a list in place with copy of elements
+    """
+    right_idx = idx + matches
+    for _ in range(matches):
+        my_list.insert(right_idx, my_list[right_idx])
+        right_idx -= 1
+
+
+def process_cards(card_list: list[str], part2=False) -> Tuple[int, int]|int:
+    """
+    >>> cards_example = [\
         'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53', \
         'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19', \
         'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1', \
         'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83', \
         'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36', \
-        'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11'])
+        'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11']
+    >>> process_cards(cards_example)
     13
+    >>> process_cards(cards_example, part2=True)
+    (13, 30)
     """
     total = 0
-    for line in card_list:
+    for idx, line in enumerate(card_list):
         card, details = line.split(':')
         winning, player = details.split('|')
         winning = set(winning.split())
         player = set(player.split())
-        total += count_card(winning, player)
+        card_value, matches = count_card(winning, player)
+        total += card_value
+        if part2:
+            update_for_matches(idx, matches, card_list)
+    if part2:
+        return total, len(card_list)
     return total
 
 if __name__ == '__main__': # pragma: no cover
