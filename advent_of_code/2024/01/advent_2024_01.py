7072ddc47535410a71c02c2f30ccd92022c44f9c
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-03 14:22:56 -0500

    ruff and filepath

--- advent_of_code/2024/01/advent_2024_01.py
+++ advent_of_code/2024/01/advent_2024_01.py
@@ -1,8 +1,7 @@
-
-def get_lists(input_file) -> tuple[list,list]:
+def get_lists(input_file) -> tuple[list, list]:
     list_a = []
     list_b = []
-    with open(input_file, 'r') as infile:
+    with open(input_file, "r") as infile:
         for line in infile:
             a, b = line.split()
             list_a.append(int(a))
@@ -10,14 +9,16 @@ def get_lists(input_file) -> tuple[list,list]:
 
     return (list_a, list_b)
 
+
 def find_total_distance(list_a, list_b):
     list_a.sort()
     list_b.sort()
     paired_list = [pair for pair in zip(list_a, list_b)]
     running_total = 0
-    for x,y in paired_list:
+    for x, y in paired_list:
         running_total += abs(y - x)
     return running_total
+
 
 def single_similarity(n, list_b):
     count = 0
@@ -26,14 +27,15 @@ def single_similarity(n, list_b):
             count += 1
     return count * n
 
+
 def calculate_similarity(list_a, list_b):
     total = 0
     for n in list_a:
         total += single_similarity(n, list_b)
     return total
 
-if __name__ == '__main__':
-    list_a, list_b = get_lists('advent_2024_01_input.txt')
-    print(find_total_distance(list_a, list_b))
-    print(calculate_similarity(list_a,list_b))
 
+if __name__ == "__main__":
+    list_a, list_b = get_lists("advent_2024_01_input.txt")
+    print(find_total_distance(list_a, list_b))
+    print(calculate_similarity(list_a, list_b))
