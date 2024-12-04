33f9efaaccb3c950bf7bf44ff28e26f141f7865c
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-04 17:54:23 -0500

    day02 part2 passing tests but failing puzzle

--- advent_of_code/2024/02/advent_2024_02.py
+++ advent_of_code/2024/02/advent_2024_02.py
@@ -2,39 +2,52 @@ def check_level_safety(level: list, dampen=False) -> bool:
     """
     Given a level return a bool of True if safe
     """
-    increasing = None
     prev_value = level[0]
+    increasing = None
+    fault = False
 
     if level[1] > prev_value:
         increasing = True
     elif level[1] < prev_value:
         increasing = False
     else:
-        return False
+        fault = True
 
-    for value in level[1::]:
+    for index, value in enumerate(level[1::]):
         # early exit
         if prev_value == value:
-            return False
-        if increasing and value < prev_value:
-            return False
-        if not increasing and value > prev_value:
-            return False
-        if abs(prev_value - value) > 3:
+            fault = True
+        elif increasing and value < prev_value:
+            fault = True
+        elif not increasing and value > prev_value:
+            fault = True
+        elif abs(prev_value - value) > 3:
+            fault = True
+        else:
+            pass
+
+        if fault and dampen:
+            recheck_list = level[:index] + level[index + 1 : :]
+            if check_level_safety(recheck_list, dampen=False) is False:
+                return False
+
+        if fault and not dampen:
             return False
 
+        fault = False
         prev_value = value
 
     return True
 
 
-def safe_level_count(report: list[list[int]]) -> int:
-    return sum([check_level_safety(level) for level in report])
+def safe_level_count(report: list[list[int]], dampen=False) -> int:
+    return sum([check_level_safety(level, dampen) for level in report])
 
 
 if __name__ == "__main__":  # pragma: no cover
-    with open("advent_2024_02_input.txt", 'r') as infile:
+    with open("advent_2024_02_input.txt", "r") as infile:
         reports = []
         for line in infile:
             reports.append([int(x) for x in line.split()])
     print(safe_level_count(reports))
+    print(safe_level_count(reports, dampen=True))
--- advent_of_code/2024/02/test_advent_2024_02.py
+++ advent_of_code/2024/02/test_advent_2024_02.py
@@ -8,18 +8,22 @@ from advent_2024_02 import check_level_safety, safe_level_count
     [
         ([7, 6, 4, 2, 1], True),
         ([1, 2, 7, 8, 9], False),
-        ([1,3,2,4,5], False),
+        ([1, 3, 2, 4, 5], False),
+        ([1, 2, 4, 5], True),
     ],
 )
 def test_check_level_safety(level, safe):
     assert check_level_safety(level) == safe
+
 
 @pytest.mark.parametrize(
     "level,safe",
     [
         ([7, 6, 4, 2, 1], True),
         ([1, 2, 7, 8, 9], False),
-        ([1,3,2,4,5], True)
+        ([9,7,6,2,1], False),
+        ([1, 3, 2, 4, 5], True),
+        ([8, 6, 4, 4, 1], True),
     ],
 )
 def test_check_level_safety_dampen(level, safe):
