a019c584176e89891a4dd121e51614b7d8c6c067
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-05 15:41:26 -0500

    testing straightness

--- advent_of_code/2024/04/test_advent_2024_04.py
+++ advent_of_code/2024/04/test_advent_2024_04.py
@@ -26,12 +26,21 @@ MXMXAXMASX
 
 EXAMPLE_AS_GRID = [row for row in EXAMPLE.split('\n') if row]
 
+
+
 def test_search_simple():
     row = 3
     col = 0
     initial_coord = (row, col)
 
-    assert search(initial_coord=initial_coord, grid=SIMPLE_EXAMPLE_AS_GRID) == 1
+    assert search(initial_coord=initial_coord, grid=SIMPLE_EXAMPLE_AS_GRID, original=initial_coord) == 1
+
+def test_must_be_straight():
+    row = 3
+    col = 0
+    initial_coord = (row, col)
+
+    assert search(initial_coord=initial_coord, grid=SIMPLE_EXAMPLE_AS_GRID, original=initial_coord) == 1
 
 
 @pytest.mark.parametrize('coord,expected',[
@@ -49,3 +58,11 @@ def test_solve_puzzle_simple():
 
 def test_solve_puzzle():
     assert solve_puzzle(EXAMPLE) == 18
+
+def test_solve_puzzle_only_straight():
+    bad_example = '''
+XVCS
+VMAC
+'''
+    assert solve_puzzle(bad_example) == 0
+    
