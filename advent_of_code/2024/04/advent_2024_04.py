a2e2fb84fc3ddaa22512a793d9b9c8a5a9fa16a1
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-05 15:41:40 -0500

    ruff fmt

--- advent_of_code/2024/04/advent_2024_04.py
+++ advent_of_code/2024/04/advent_2024_04.py
@@ -1,15 +1,17 @@
 from itertools import product
 
-SEARCH_WORD = 'XMAS'
+SEARCH_WORD = "XMAS"
+
 
 def get_neighbor_coord(row, col):
     possibilities = set()
     for r in range(row - 1, row + 2):
         for c in range(col - 1, col + 2):
-            possibilities.add((r,c))
-    possibilities.remove((row,col))
+            possibilities.add((r, c))
+    possibilities.remove((row, col))
 
     return possibilities
+
 
 def get_neighbors(coord: tuple[int, int], grid: list[list[str]]):
     max_row = len(grid)
@@ -24,15 +26,19 @@ def get_neighbors(coord: tuple[int, int], grid: list[list[str]]):
         if 0 > col or col >= max_col:
             continue
         neighbor = grid[row][col]
-        neighbors.add(
-            (neighbor,(row,col))
-        )
+        neighbors.add((neighbor, (row, col)))
     return neighbors
 
-def search(initial_coord: tuple[int,int], grid: list[list[str]], original, word='XMAS', ) -> int:
+
+def search(
+    initial_coord: tuple[int, int],
+    grid: list[list[str]],
+    original,
+    word="XMAS",
+) -> int:
     try:
         target_letter = word[1]
-    except IndexError: #hit the end
+    except IndexError:  # hit the end
         init_row, init_col = initial_coord
         orig_row, orig_col = original
 
@@ -45,7 +51,7 @@ def search(initial_coord: tuple[int,int], grid: list[list[str]], original, word=
         if col_distance == 3 and init_row == orig_row:
             return 1
         return 0
-    
+
     neighbors = get_neighbors(coord=initial_coord, grid=grid)
     candidates = []
 
@@ -56,18 +62,25 @@ def search(initial_coord: tuple[int,int], grid: list[list[str]], original, word=
         return 0
     else:
         return sum(
-            [search(initial_coord=coord, grid=grid, word=word[1::], original=original) for coord in candidates]
+            [
+                search(
+                    initial_coord=coord, grid=grid, word=word[1::], original=original
+                )
+                for coord in candidates
+            ]
         )
 
 
 def solve_puzzle(puzzle_str: str) -> int:
-    grid = [row for row in puzzle_str.split('\n') if row]
+    grid = [row for row in puzzle_str.split("\n") if row]
 
     successes = 0
     for r_index, row in enumerate(grid):
         for col_index, char in enumerate(row):
-            if char == 'X':
-                result = search((r_index,col_index), grid=grid, original=(r_index,col_index))
+            if char == "X":
+                result = search(
+                    (r_index, col_index), grid=grid, original=(r_index, col_index)
+                )
                 successes += result
 
     return successes
