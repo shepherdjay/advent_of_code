e49c30421122fab7a850fe12c5fd5df9242c4d18
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-05 13:33:54 -0500

    small distraction of creating a cookiecutter

---
+++ advent_of_code/newday.py
@@ -0,0 +1,34 @@
+import requests
+from urllib.parse import urlparse
+from pathlib import Path
+import aocd
+import os
+
+def fetch_input_example(year, day):
+    data = aocd.get_data(day=day, year=year)
+    example = aocd.get
+    if data.startswith('Puzzle inputs differ'):
+        raise RuntimeError("Unable to fetch puzzle input, check session exists")
+
+
+if __name__ == '__main__':
+    get_base_url = input("Enter day url: ")
+
+    year, _, day = urlparse(get_base_url).path.split('/')[1:]
+    day = int(day)
+    year = int(year)
+
+    basepath = Path(f'./{year}/{day:02}')
+    basepath.mkdir(parents=True, exist_ok=True)
+
+    with open(f'{basepath}/advent_{year}_{day:02}.py', 'w') as newfile:
+        pass
+
+    with open(f'{basepath}/test_advent_{year}_{day:02}.py', 'w') as newfile:
+        pass
+
+    with open(f'{basepath}/advent_{year}_{day:02}_input.txt', 'w') as newfile:
+        newfile.write(fetch_input_example(year=year,day=day))
+
+
+
