02d31b3ab5d72e6d7fe5ed53c2eeb7c683c1cd5b
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-06 11:58:55 -0500

    checking printjobs

--- advent_of_code/2024/05/advent_2024_05.py
+++ advent_of_code/2024/05/advent_2024_05.py
@@ -10,16 +10,31 @@ def split_input(puzzle_input: str) -> tuple[dict[int : list[int]], list[list[int
         page_string = line.split(",")
 
         if len(rules_string) == 2:
-            print(rules_string)
             page, before = [int(x) for x in line.split("|")]
             rules[page].append(before)
         if len(page_string) > 2:
             pages_to_produce.append([int(x) for x in line.split(",")])
     return rules, pages_to_produce
 
+def check_printjob(printjob: list[int], rules: dict[int:list[int]]):
+    checked_values = []
+
+    for value in printjob:
+        value_rules = rules.get(value, list())
+        for following_page in value_rules:
+            if following_page in checked_values:
+                return False
+        checked_values.append(value)
+    
+    return True
+
+
 
 def solve_puzzle(puzzle_input):
-    pass
+    rules, pages_to_produce = split_input(puzzle_input=puzzle_input)
+
+    middle_values = [pages[(len(pages)-1)//2] for pages in pages_to_produce]
+    return sum(middle_values)
 
 
 if __name__ == "__main__":  # pragma: no cover
