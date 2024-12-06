2a609f94bf7bc8492af344d16f96812ba452fe09
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-06 11:45:30 -0500

    day05 part1 -- initial testing

---
+++ advent_of_code/2024/05/test_advent_2024_05.py
@@ -0,0 +1,51 @@
+import pytest
+from advent_2024_05 import split_input, solve_puzzle
+
+EXAMPLE = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+"""
+
+def test_split_input():
+    simple_split = """
+47|53
+
+97,12,42
+"""
+    expected_dict = {47: [53]}
+    expected_list = [[97, 12, 42]]
+
+    actual_dict, actual_list = split_input(simple_split)
+
+    assert actual_dict == expected_dict
+    assert actual_list == expected_list
+
+
+def test_solve_puzzle():
+    assert solve_puzzle(EXAMPLE) == 143
