57f19554752834c2057e477266dbaa17b7b5f4da
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-07 11:15:21 +0000

    day07 part1 puzzle input failing, example working

--- advent_of_code/2024/07/advent_2024_07.py
+++ advent_of_code/2024/07/advent_2024_07.py
@@ -1,6 +1,7 @@
 from collections import deque
 
-def solve_layer(target, values: deque, cur_number = 0):
+
+def solve_layer(target, values: deque, cur_number=0):
     n1 = values.popleft()
 
     add = n1 + cur_number
@@ -12,19 +13,37 @@ def solve_layer(target, values: deque, cur_number = 0):
         return True
     else:
         if values:
-            return any([solve_layer(target, values.copy(), cur_number=operation) for operation in [add, multi]])
+            return any(
+                [
+                    solve_layer(target, values.copy(), cur_number=operation)
+                    for operation in [add, multi]
+                ]
+            )
     return False
 
-def solve_puzzle(puzzle_input:str) -> int:
+
+def solve_puzzle(puzzle_input: str) -> int:
     total = 0
 
     lines = puzzle_input.splitlines()
     for line in lines:
         if line:
-            target_number, values = line.strip().split(':')
+            target_number, values = line.strip().split(":")
             target_number = int(target_number)
             values = deque([int(value) for value in values.split()])
             if solve_layer(target=target_number, values=values):
                 total += target_number
-    
+
     return total
+
+
+if __name__ == "__main__":  # pragma: no cover
+    from aocd import submit
+
+    with open("advent_2024_07_input.txt") as infile:
+        puzzle_input = infile.read()
+
+    part_a = solve_puzzle(puzzle_input)
+    print(part_a)
+
+    submit(part_a, part="a")
