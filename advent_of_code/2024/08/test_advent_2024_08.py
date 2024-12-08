dd848a5d9964d4c5c21f7a3e42382c618c0cbd0f
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-08 16:38:41 +0000

    day08 part1 solved

---
+++ advent_of_code/2024/08/test_advent_2024_08.py
@@ -0,0 +1,66 @@
+import pytest
+from hypothesis import given, strategies as st
+
+import advent_2024_08 as advent
+
+EXAMPLE = """
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+"""
+
+
+@st.composite
+def aninode_diagonal(draw):
+    initial = draw(st.tuples(st.integers(min_value=0), st.integers(min_value=0)))
+    aninode = (initial[0] * 2, initial[1] * 2)
+
+    return initial, aninode
+
+
+@given(aninode_diagonal())
+def test_calculate_double_property(sample):
+    initial, aninode = sample
+    orig_coord = (0, 0)  # zzero index for hypothesis
+
+    advent.calculate_double(orig_coord, initial) == aninode
+
+
+@pytest.mark.parametrize(
+    "orig_coord, target_coord, expected",
+    [
+        ((1, 1), (3, 3), (5, 5)),
+        ((1, 0), (3, 3), (5, 6)),
+        ((5, 5), (4, 4), (3, 3)),
+    ],
+)
+def test_calculate_double(orig_coord, target_coord, expected):
+    assert advent.calculate_double(orig_coord, target_coord) == expected
+
+
+def test_at_anninode():
+    antenna_map = {"A": {(3, 3), (5, 5)}}
+    assert advent.at_anninode((1, 1), antenna_map)
+    assert not advent.at_anninode((1, 0), antenna_map)
+
+
+def test_solve_puzzle_simple():
+    puzzle_input = """
+    ....
+    .AA.
+    """
+
+    assert advent.solve_puzzle(puzzle_input) == 2
+
+
+def test_solve_puzzle_example():
+    assert advent.solve_puzzle(EXAMPLE) == 14
