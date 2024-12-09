a2c60226e118bd5820794e3dffb07a8663e5a450
Jay Shepherd <shepherdjay@users.noreply.github.com>
2024-12-09 14:00:45 -0500

    i went down a weird rabbit hole here... lost the plot

--- advent_of_code/2024/09/test_2024day09.py
+++ advent_of_code/2024/09/test_2024day09.py
@@ -2,12 +2,18 @@ import y2024day09 as advent
 
 
 def test_filesystem():
-    input = '123'
+    input = "123"
 
-    assert advent.construct_filesystem(input) == ['0','.','.','1','1','1']
+    assert advent.construct_filesystem(input) == ["0", ".", ".", "1", "1", "1"]
+
+def test_filesystem_class():
+    input = "123"
+
+    assert list(advent.FileSystem.from_string(input).describe()) == ["0", ".", ".", "1", "1", "1"]
+
 
 def test_solve_example():
-    input = '2333133121414131402'
+    input = "2333133121414131402"
     assert advent.solve_puzzle(input) == 1928
 
 
@@ -17,7 +23,8 @@ def test_filesystem_class_add():
 
     filesystem.add_file(file, loc=2)
 
-    assert list(filesystem.describe()) == ['.', '.', 50, 50, '.']
+    assert list(filesystem.describe()) == [".", ".", 50, 50, "."]
+
 
 def test_filesystem_class_add_fragment():
     filesystem = advent.FileSystem()
@@ -27,4 +34,33 @@ def test_filesystem_class_add_fragment():
     filesystem.add_file(file, loc=2)
 
     assert list(filesystem.describe()) == [1, 1, 50, 1, 50]
-    assert filesystem.fragmented
+
+
+def test_filesystem_class_add_dnf():
+    filesystem = advent.FileSystem()
+    filesystem._disk = [1, 1, None, 1, None, None]
+    file = advent.File(name=50, size=2)
+
+    filesystem.add_file(file, dnf=True)
+    assert list(filesystem.describe()) == [1, 1, ".", 1, 50, 50]
+
+
+def test_filesystem_find_free_space_base_case():
+    filesystem = advent.FileSystem()
+    filesystem._disk = [None]
+
+    assert filesystem.find_free_space(1) == 0
+
+
+def test_filesystem_find_free_space():
+    filesystem = advent.FileSystem()
+    filesystem._disk = [1, 1, None, 1, None, None]
+
+    assert filesystem.find_free_space(2) == 4
+
+
+def test_filesystem_find_free_space_start():
+    filesystem = advent.FileSystem()
+    filesystem._disk = [1, 1, None, None, None, None]
+
+    assert filesystem.find_free_space(2, 4) == 4
--- advent_of_code/2024/09/y2024day09.py
+++ advent_of_code/2024/09/y2024day09.py
@@ -4,30 +4,49 @@ from dataclasses import dataclass
 BASEPATH = Path(__file__).parent.resolve()
 
 
-
-
 @dataclass
 class File:
     name: int
     size: int
-    def __str__(self):
-        return f'{self.name}'
 
-class FileSystem():
-    def __init__(self, disk_size: int=0):
+    def __str__(self):
+        return f"{self.name}"
+
+
+class FileSystem:
+    def __init__(self, disk_size: int = 0):
         self._disk = [None for _ in range(disk_size)]
         self.fragmented = False
 
-    def add_file(self, file: File, loc: int=None):
+    def find_free_space(self, desired_space: int, _start_idx=0):
+        print(f"finding {desired_space}")
+        free_sectors = 0
+        for i, value in enumerate(self._disk):
+            if i >= _start_idx:
+                if value is None:
+                    free_sectors += 1
+                else:
+                    free_sectors = 0
+                if free_sectors == desired_space:
+                    return i - desired_space + 1
+
+    def add_file(self, file: File, loc: int = 0, dnf=False):
+        if dnf:
+            space_needed = file.size
+        else:
+            space_needed = 1
+
         allocated = 0
-        i = 0
-        while allocated != file.size:
-            if not self._disk[loc + i]:
-                self._disk[loc + i] = file
-                allocated += 1
+        start = self.find_free_space(space_needed, _start_idx=loc)
+        while True:
+            self._disk[start] = file
+            allocated += 1
+            if allocated == file.size:
+                break
+            if dnf:
+                start += 1
             else:
-                self.fragmented = True
-            i += 1
+                start = self.find_free_space(max(1, (space_needed - allocated)), _start_idx=loc)
 
     def describe(self):
         for sector in self._disk:
@@ -36,20 +55,34 @@ class FileSystem():
             elif sector:
                 yield sector
             else:
-                yield '.'
-        
-        
+                yield "."
+
+    @classmethod
+    def from_string(cls, description_string) -> 'FileSystem':
+        file_system = cls()
+        print(file_system)
+        file_index = 0
+        location = 0
+        for i, value in enumerate(description_string):
+            if (i + 1) % 2 == 0:
+                file_system._disk += [None] * int(value)
+            else:
+                file_system._disk += [f"{file_index}"] * int(value)
+                file_index += 1
+        return file_system
+
 
 def construct_filesystem(puzzle_input):
     file_system = []
     file_index = 0
     for i, value in enumerate(puzzle_input):
         if (i + 1) % 2 == 0:
-            file_system += ['.'] * int(value)
+            file_system += ["."] * int(value)
         else:
-            file_system += [f'{file_index}'] * int(value)
+            file_system += [f"{file_index}"] * int(value)
             file_index += 1
     return file_system
+
 
 def solve_puzzle(puzzle_input, part2=False):
     filesystem = construct_filesystem(puzzle_input)
@@ -58,20 +91,18 @@ def solve_puzzle(puzzle_input, part2=False):
 
     new_filesystem = []
     while left_idx <= right_idx:
-        if filesystem[left_idx] != '.':
+        if filesystem[left_idx] != ".":
             new_filesystem.append(int(filesystem[left_idx]))
             left_idx += 1
         else:
-            if filesystem[right_idx] != '.':
+            if filesystem[right_idx] != ".":
                 new_filesystem.append(int(filesystem[right_idx]))
                 right_idx -= 1
                 left_idx += 1
             else:
                 right_idx -= 1
-    
-    return sum([i * x for i,x in enumerate(new_filesystem)])
-        
 
+    return sum([i * x for i, x in enumerate(new_filesystem)])
 
 
 if __name__ == "__main__":  # pragma: no cover
@@ -79,7 +110,7 @@ if __name__ == "__main__":  # pragma: no cover
     from aocd.exceptions import AocdError
 
     with open(f"{BASEPATH}/input.txt") as infile:
-        puzzle_input = infile.read().strip('\n')
+        puzzle_input = infile.read().strip("\n")
 
     part_a = solve_puzzle(puzzle_input)
     print(part_a)
@@ -88,7 +119,7 @@ if __name__ == "__main__":  # pragma: no cover
     print(part_b)
 
     try:
-        with open(f'../../../.token') as infile:
+        with open(f"../../../.token") as infile:
             session = infile.read().strip()
         submit(part_a, part="a", session=session)
         # submit(part_b, part="b", session=session)
